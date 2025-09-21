e081439a365debf2cb32770fae545d73ff2c0937
Farzad <Farzadsarfarazwvo@gmail.com>
2025-09-21 19:10:59 +0200

    Complete calculator testing assignment

--- exercice/calculator/test_calculator.py
+++ exercice/calculator/test_calculator.py
@@ -9,10 +9,8 @@ def calculator():
 def test_add(calculator):
     assert calculator.add(2, 3) == 5
 
-
 def test_subtract(calculator):
     assert calculator.subtract(5, 3) == 2
-
 
 def test_multiply(calculator):
     assert calculator.multiply(4, 3) == 12
